ec85189a01a1f46ad617d76120c76ea905b2dc9e
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-09-24 10:18:06 +0700

    modify response,request model

--- koi-farm-api/Repository/Model/Blog/ResponseBlogModel.cs
+++ koi-farm-api/Repository/Model/Blog/ResponseBlogModel.cs
@@ -12,5 +12,6 @@ namespace Repository.Model.Blog
         public string Title { get; set; }
 
         public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
