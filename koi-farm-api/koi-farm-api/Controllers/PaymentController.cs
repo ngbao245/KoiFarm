b8f8e300ffa9b9a53b8ef189b5db92d5b58fb819
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-10-24 21:28:33 +0700

    add api for requesting refund

--- koi-farm-api/koi-farm-api/Controllers/PaymentController.cs
+++ koi-farm-api/koi-farm-api/Controllers/PaymentController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Repository.Data.Entity;
 using Repository.Model;
 using Repository.Model.Order;
@@ -29,6 +30,11 @@ namespace koi_farm_api.Controllers
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _configuration = configuration;
+        }
+
+        private string GetUserIdFromClaims()
+        {
+            return User.FindFirst("UserID")?.Value;
         }
 
         // POST method to create a payment request and generate VNPAY payment URL
@@ -228,6 +234,78 @@ namespace koi_farm_api.Controllers
                 StatusCode = 200,
                 Data = responsePayments
             });
+        }
+
+
+
+        [HttpPost("process-refund")]
+        public IActionResult ProcessRefund([FromBody] RefundRequestModel refundRequest)
+        {
+            var payment = _unitOfWork.PaymentRepository.GetById(refundRequest.TransactionNo);
+            if (payment == null)
+            {
+                return NotFound(new ResponseModel
+                {
+                    StatusCode = 404,
+                    MessageError = "Payment not found."
+                });
+            }
+            
+            if (refundRequest == null || string.IsNullOrEmpty(payment.OrderId))
+            {
+                return BadRequest(new ResponseModel
+                {
+                    StatusCode = 400,
+                    MessageError = "Invalid refund request model or missing OrderId."
+                });
+            }
+
+            var order = _unitOfWork.OrderRepository.GetById(payment.OrderId);
+            if (order == null)
+            {
+                return NotFound(new ResponseModel
+                {
+                    StatusCode = 404,
+                    MessageError = "Order not found."
+                });
+            }
+
+            if (order.Status != "Cancelled")
+            {
+                return BadRequest(new ResponseModel
+                {
+                    StatusCode = 400,
+                    MessageError = "This order has not been cancelled"
+                });
+            }
+
+            var userId = GetUserIdFromClaims();
+            var user = _unitOfWork.UserRepository.GetById(userId);
+
+            try
+            {
+                var response = _vnPayService.ProcessRefund(refundRequest, HttpContext, payment.CreatedTime, payment.Amount, user.Name, payment.OrderId);
+
+                if (response.ResponseCode == "00")
+                {
+                    payment.Status = "Refunded";
+                    _unitOfWork.PaymentRepository.Update(payment);
+                }
+
+                return Ok(new ResponseModel
+                {
+                    StatusCode = 200,
+                    Data = response
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResponseModel
+                {
+                    StatusCode = 500,
+                    MessageError = "Failed to process refund: " + ex.InnerException
+                });
+            }
         }
     }
 }
