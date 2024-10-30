2b82a0d4b840d1536f62ce77961db88d00405bef
BaoBiBo <ng.hoangbao03@gmail.com>
2024-10-30 08:53:45 +0700

    update getCertificateById response

--- koi-farm-api/koi-farm-api/Controllers/CertificateController.cs
+++ koi-farm-api/koi-farm-api/Controllers/CertificateController.cs
@@ -78,7 +78,8 @@ public class CertificateController : ControllerBase
             ImageUrl = certificate.ImageUrl,
             ProductCertificates = certificate.CertificateProduct.Select(item => new
             {
-                ItemId = item.Id,
+                ProductItemId = item.ProductItemId,
+                Id = item.Id,
                 Provider = item.Provider,
                 PublishDate = item.CreatedTime
             }).ToList()
